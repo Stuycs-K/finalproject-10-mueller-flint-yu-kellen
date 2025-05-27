f9228cb7ee0de426abdc5a14a9217470ca6cee57
ONLYIWIN21 <flintthecuber@gmail.com>
2025-05-27 15:01:47 -0400

    file perms

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,21 @@
 mod io;
 mod file;
 
+use std::fs::OpenOptions;
+
 use crate::io::*;
 use crate::file::*;
 
 fn main() {
     let args = std::env::args().collect::<Vec<_>>();
     let file = args.get(1).expect("Please provide a file name");
-    let file = std::fs::File::open(file).expect("Failed to open file");
+    let file = OpenOptions::new()
+        .read(true)
+        .write(true)
+        .create(true)
+        .truncate(false)
+        .open(file)
+        .expect("Failed to open file");
     let mut text = load(&file);
 
     while handle_input(&mut text) {
