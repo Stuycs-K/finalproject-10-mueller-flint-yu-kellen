205fcedeb201db216d3bd260a2ae15fb2a26c41c
Kellen Yu <thanoscarthanoscar420420@gmail.com>
2025-05-29 14:14:15 -0400

    fix merge conflict

--- src/file/mod.rs
+++ src/file/mod.rs
@@ -1,8 +1,13 @@
 use std::fs::File;
+use std::io::Read;
 
 pub fn save(file: File, text: &str) {
 }
 
-pub fn load(file: &File) -> String {
-    String::new()
+// Load a file and convert to hex file
+pub fn load(file: &mut File) -> String {
+    let mut out = String::new();
+    let mut data: Vec<u8> = Vec::<u8>::new();
+    file.read_to_end(&mut data);
+    return out;
 }
