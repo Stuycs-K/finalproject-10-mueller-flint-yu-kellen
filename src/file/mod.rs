2c402e247475e39f18f005602b7487f287dbf7e9
Kellen Yu <thanoscarthanoscar420420@gmail.com>
2025-05-29 14:34:50 -0400

    hex string to bytes conversion

--- src/file/mod.rs
+++ src/file/mod.rs
@@ -1,7 +1,11 @@
 use std::fs::File;
 use std::io::Read;
 
-pub fn save(file: File, text: &str) {
+
+pub fn save(file: &mut File, text: &str) {
+    let mut data: Vec<u8> = text.split_whitespace().map(|s| u8::from_str_radix(s, 16).unwrap()).collect::<Vec<u8>>();
+
+
 }
 
 // Load a file and convert to hex file
@@ -13,11 +17,11 @@ pub fn load(file: &mut File) -> String {
     let mut i = 0;
     while i < data.len() {
         let mut s = format!("{:x}", data[i]);
-        if (s.len() < 2) {
+        if s.len() < 2 {
             s.insert(0, '0');
         }
         out.push_str(&s);
-        if (i < data.len()-1) {
+        if i < data.len()-1 {
             out.push(' ');
         }
         i += 1;
