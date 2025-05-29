91fe445485b89245f0e628cc814bbb4381eabcdb
Kellen Yu <thanoscarthanoscar420420@gmail.com>
2025-05-29 14:28:47 -0400

    got hex conversion to work

--- src/file/mod.rs
+++ src/file/mod.rs
@@ -12,7 +12,14 @@ pub fn load(file: &mut File) -> String {
     
     let mut i = 0;
     while i < data.len() {
-        out.push_str(&format!("{:x}", data[i]));
+        let mut s = format!("{:x}", data[i]);
+        if (s.len() < 2) {
+            s.insert(0, '0');
+        }
+        out.push_str(&s);
+        if (i < data.len()-1) {
+            out.push(' ');
+        }
         i += 1;
     }
     println!("{}", out);
